29f7d51792f997429a475e3970a4b6c818fe508c
Pienskoi <vladymyrpenskoy@gmail.com>
2021-12-07 21:46:09 +0200

    Lab3: Copy Lab2 functionality

---
+++ Lab3/app/src/main/java/com/pienskoi/lab3/AlertFragment.kt
@@ -0,0 +1,18 @@
+package com.pienskoi.lab3
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+
+class AlertFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setMessage("Будь-ласка заповніть текстове поле та оберіть всі наявні опції!")
+            .setPositiveButton(getString(R.string.button_ok)) { dialog, _ -> dialog.cancel() }
+            .create()
+
+    companion object {
+        const val TAG = "AlertDialog"
+    }
+}
